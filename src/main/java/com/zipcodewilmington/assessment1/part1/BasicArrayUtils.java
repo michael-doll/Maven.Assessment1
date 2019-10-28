6b35bd370908a84d6d261c5f4436dcecebed6430
michael-doll <MWDoll88@gmail.com>
2019-10-28 09:44:26 -0400

    Save work

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicArrayUtils.java
@@ -2,37 +2,31 @@ package com.zipcodewilmington.assessment1.part1;
 
 /**
  * Created by leon on 2/16/18.
+ * Michael Doll
+ * 10.28.2019
  */
 public class BasicArrayUtils {
     /**
      * @param stringArray an array of String objects
      * @return the first element in the array
      */
-    public static String getFirstElement(String[] stringArray) {
-        return null;
-    }
+    public static String getFirstElement(String[] stringArray) { return stringArray[0]; }
 
     /**
      * @param stringArray an array of String objects
      * @return the second element in the array
      */
-    public static String getSecondElement(String[] stringArray) {
-        return null;
-    }
+    public static String getSecondElement(String[] stringArray) { return stringArray[1]; }
 
     /**
      * @param stringArray an array of String objects
      * @return the last element in the array
      */
-    public static String getLastElement(String[] stringArray) {
-        return null;
-    }
+    public static String getLastElement(String[] stringArray) { return stringArray[stringArray.length-1]; }
 
     /**
      * @param stringArray an array of String objects
      * @return the second to last element in the array
      */
-    public static String getSecondToLastElement(String[] stringArray) {
-        return null;
-    }
+    public static String getSecondToLastElement(String[] stringArray) { return stringArray[stringArray.length-2]; }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.assessment1.part1;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -9,7 +11,8 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        String upper = str.toUpperCase();
+        return upper.charAt(0) + str.substring(1);
     }
 
     /**
@@ -17,7 +20,8 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        StringBuilder strSb = new StringBuilder(str);
+        return strSb.reverse().toString();
     }
 
     /**
@@ -25,7 +29,8 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+        String strRev = reverse(str);
+        return camelCase(strRev);
     }
 
 
@@ -34,7 +39,7 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+        return str.substring(1,str.length()-1);
     }
 
     /**
@@ -42,6 +47,15 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+       char [] strCharArr = str.toCharArray();
+        for(int i = 0; i < strCharArr.length; i++){
+            char current = strCharArr[i];
+            if(Character.isUpperCase(current)){
+                strCharArr[i] = Character.toLowerCase(current);
+            }else{
+                strCharArr[i] = Character.toUpperCase(current);
+            }
+        }
+        return new String(strCharArr);
     }
 }
