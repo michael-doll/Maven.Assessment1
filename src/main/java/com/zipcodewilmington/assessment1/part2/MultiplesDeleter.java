d35128a39041cd521dbf59240da25899742386a4
michael-doll <MWDoll88@gmail.com>
2019-10-28 14:13:45 -0400

    save work, complete part1,part2

--- src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
@@ -13,13 +13,8 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with evens removed
      */
     public Integer[] deleteEvens(Integer[] ints){
-        ArrayList<Integer> intsList = (ArrayList<Integer>) Arrays.asList(ints);
-        for(int i =0; i < intsList.size(); i++){
-            if(intsList.get(i) %2 != 0){
-                intsList.remove(intsList.get(i));
-            }
-        }
-        return (Integer[])intsList.toArray();
+        return (deleteMultiplesOfN(ints,2));
+
     }
 
     /**
@@ -28,7 +23,17 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with odds removed
      */
     public Integer[] deleteOdds(Integer[] ints) {
-        return null;
+        ArrayList evens = new ArrayList();
+        for(int i =0 ; i < ints.length; i++){
+            if(ints[i] %2==0){
+                evens.add(ints[i]);
+            }
+        }
+        Integer [] returnArr = new Integer[evens.size()];
+        for(int i =0; i < returnArr.length; i++){
+            returnArr[i]= (Integer)evens.get(i);
+        }
+        return returnArr;
     }
 
     /**
@@ -37,7 +42,7 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by 3 removed
      */
     public Integer[] deleteMultiplesOf3(Integer[] ints) {
-        return null;
+        return (deleteMultiplesOfN(ints,3));
     }
 
     /**
@@ -47,6 +52,17 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by `multiple` removed
      */
     public Integer[] deleteMultiplesOfN(Integer[] ints, int multiple) {
-        return null;
+
+        ArrayList leftovers = new ArrayList();
+        for(int i =0 ; i < ints.length; i++){
+            if(ints[i] %multiple!=0){
+                leftovers.add(ints[i]);
+            }
+        }
+        Integer [] returnArr = new Integer[leftovers.size()];
+        for(int i =0; i < returnArr.length; i++){
+            returnArr[i]= (Integer)leftovers.get(i);
+        }
+        return returnArr;
     }
 }
